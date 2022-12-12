7b43ba6e456802dedc2d8ccaf948a977d5a27053
yookeunbyul <91243651+yookeunbyul@users.noreply.github.com>
2022-12-12 16:57:24 +0900

    11장: 컴포넌트 성능 최적화

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import TodoListItem from './TodoListItem';
 import styled from 'styled-components';
+import { List } from 'react-virtualized';
 
 const TodoList = ({ todos, onRemove, onToggle }) => {
-  return (
-    <List>
-      {todos.map((todo) => (
+  const rowRenderer = useCallback(
+    ({ index, key, style }) => {
+      const todo = todos[index];
+      return (
         <TodoListItem
           todo={todo}
-          key={todo.id}
+          key={key}
           onRemove={onRemove}
           onToggle={onToggle}
+          style={style}
         />
-      ))}
-    </List>
+      );
+    },
+    [onRemove, onToggle, todos],
+  );
+  return (
+    <StyledList
+      width={512}
+      height={513}
+      rowCount={todos.length}
+      rowHeight={57}
+      rowRenderer={rowRenderer}
+      list={todos}
+      style={{ outline: 'none' }}
+    />
   );
 };
 
-const List = styled.div`
+const StyledList = styled(List)`
   min-height: 320px;
   max-height: 513px;
   overflow-y: auto;
 `;
 
-export default TodoList;
+export default React.memo(TodoList);
--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -6,29 +6,38 @@ import {
   MdRemoveCircleOutline,
 } from 'react-icons/md';
 
-const TodoListItem = ({ todo, onRemove, onToggle }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
   const { id, text, checked } = todo;
   return (
-    <ListItem>
-      <CheckBox checked={checked} onClick={() => onToggle(id)}>
-        {checked ? <StyledMdCheckBox /> : <StyledMdCheckBoxOutlineBlank />}
-        <div className="text">{text}</div>
-      </CheckBox>
-      <div className="remove" onClick={() => onRemove(id)}>
-        <MdRemoveCircleOutline />
-      </div>
-    </ListItem>
+    <ListWrap style={style}>
+      <ListItem>
+        <CheckBox checked={checked} onClick={() => onToggle(id)}>
+          {checked ? <StyledMdCheckBox /> : <StyledMdCheckBoxOutlineBlank />}
+          <div className="text">{text}</div>
+        </CheckBox>
+        <div className="remove" onClick={() => onRemove(id)}>
+          <MdRemoveCircleOutline />
+        </div>
+      </ListItem>
+    </ListWrap>
   );
 };
+
+const ListWrap = styled.div`
+  /* 엘리먼트 사이 */
+  & + & {
+    border-top: 1px solid #dee2e6;
+
+    &:nth-child(even) {
+      background: #f8f9fa;
+    }
+  }
+`;
 
 const ListItem = styled.div`
   padding: 1rem;
   display: flex;
   align-items: center;
-
-  &:nth-child(even) {
-    background: #f8f9fa;
-  }
 
   .remove {
     display: flex;
@@ -39,11 +48,6 @@ const ListItem = styled.div`
     &:hover {
       color: #ff8787;
     }
-  }
-
-  /* 엘리먼트 사이 */
-  & + & {
-    border-top: 1px solid #dee2e6;
   }
 `;
 
@@ -70,4 +74,5 @@ const StyledMdCheckBox = styled(MdCheckBox)`
   color: #22b8cf;
 `;
 
-export default TodoListItem;
+// 컴포넌트의 props가 바뀌지않았따면, 리렌더링하지 않음
+export default React.memo(TodoListItem);
